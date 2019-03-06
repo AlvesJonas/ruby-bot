d6b30c23a5ee1fc3005a11f13e40534db2de83d2
Jonas <jonas.alves@live.com>
2019-03-06 20:34:13 -0300

    Added translate service drafts

--- app/services/language/translate_service.rb
+++ app/services/language/translate_service.rb
@@ -1,5 +1,24 @@
+require 'rest-client'
+require 'json'
+
 module LanguageModule
   class TranslateService
+    def initialize(params)
+      @word = params["word"]
+      @target = params["target"]
+    end 
 
+    def call
+      begin
+        yandex_api_url = "https://translate.yandex.net/api/v1.5/tr.json/translate"
+        yandex_api_key = ENV['YANDEX_KEY']
+
+        url = "#{yandex_api_url}?key=#{yandex_api_key}&text=#{@word}&lang=#{@target}"
+        res = RestClient.get url
+        JSON.parse(res.body)['text'][0]
+      rescue RestClient::ExceptionWithResponse => e
+        e.response
+      end
+    end
   end
 end
